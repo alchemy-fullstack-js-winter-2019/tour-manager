5409261f66d108d90bd711a58ffd52d9a8febad6
Shaba9 <shaba92@gmail.com>
2019-02-03 17:12:44 -0800

    get tours by id passes

--- test/routes/tours.test.js
+++ test/routes/tours.test.js
@@ -3,6 +3,8 @@ const app = require('../../lib/app');
 const mongoose = require('mongoose');
 const connect = require('../../lib/utils/connect');
 const seedData = require('../seedData');
+const Stop = require('../../lib/models/Stop');
+const Tour = require('../../lib/models/Tour');
 
 describe('tours route', () => {
   beforeAll(() => connect());
@@ -34,5 +36,61 @@ describe('tours route', () => {
     return request(app)
       .get('/tours')
       .then(res => expect(res.body).toHaveLength(10));
+  });
+
+  it('can get a tour by id', () => {
+    return Stop
+      .create({
+        location: {
+          city: 'Wilsonville',
+          state: 'OR',
+          zip: 97070
+        },
+
+        weather: {
+          temperature: 40,
+          wind: 10,
+          airQ: 'good'
+        },
+
+        attendance: 45
+      })
+      .then(stop => {
+        return Tour
+          .create({
+            title: 'Circus 2019',
+            activities: ['Juggling', 'Twisting', 'Animal weirdness'],
+            stops: [stop._id]
+          });
+      })
+      .then(tour => {
+        return request(app)
+          .get(`/tours/${tour._id}`);
+      })
+      .then(res => expect(res.body).toEqual({
+        title: 'Circus 2019',
+        activities: ['Juggling', 'Twisting', 'Animal weirdness'],
+        launchDate: expect.any(String),
+        stops: [
+          {
+            location: {
+              city: 'Wilsonville',
+              state: 'OR',
+              zip: 97070
+            },
+    
+            weather: {
+              temperature: 40,
+              wind: 10,
+              airQ: 'good'
+            },
+    
+            attendance: 45,
+            _id: expect.any(String)
+          },
+        ],
+        _id: expect.any(String),
+        __v: 0
+      }));
   });
 });
